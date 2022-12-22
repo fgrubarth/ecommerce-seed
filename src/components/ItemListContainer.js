c250588d1d3081280c7b69388fe36ef61e445327
fgrubarth <fgrubart@gmail.com>
2022-12-22 16:18:19 -0300

    corregí errores

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -20,32 +20,3 @@ export const ItemListContainer = () => {
 };
 
 
-//   useEffect(() => {
-//     new Promise((resolve) =>
-//       setTimeout(() => {
-//         resolve(item);
-//       }, 2000)
-//     ).then((data) => {
-//       if (category) {
-//         const categories = data.filter(
-//           (product) => product.category === category
-//         );
-//         setProducts(categories);
-//       } else {
-//         setProducts(data);
-//       }
-//     });
-//   }, [category]);
-
-//   if (products.length === 0) {
-//     return <p>Loading...</p>;
-//   }
-
-//   return (
-//     <div className="h-full">
-//       <ItemList products={products} />
-//     </div>
-//   );
-// };
-
-// export default ItemListContainer;
